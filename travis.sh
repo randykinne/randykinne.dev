a76c6d81f689fec47b7e3cdfb9e39202ace30714
Randy Kinne <r27kinne@gmail.com>
2020-02-09 16:01:56 -0500

    [Travis] Add bundle install
    #1

--- travis.sh
+++ travis.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-#set -e # halt script on error
+set -e # halt script on error
 
 echo 'Testing travis...'
+bundle install
 bundle exec jekyll build
 bundle exec htmlproofer ./_site --only-4xx
